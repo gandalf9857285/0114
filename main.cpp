b86de854c3b306e8849bb7270ff5226ba03d5094
Valentin Romanov <valentin1.romanov@yandex.ru>
2022-01-14 11:22:55 +0700

    структуры2

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ struct Date{
 void print (Date d){
     std::count<<d.day<<"."<<d.month<<"."<<d.year << std::enl;
 }
+void tommorow (Date d){
+    std::count<<d.day<<"."<<d.month<<"."<<d.year << std::enl;
+}
 
 int main(int argc, char *argv[])
 {
@@ -17,7 +20,15 @@ int main(int argc, char *argv[])
 	x.day = 14;
 	x.month = 1;
 	x.year = 2022;
-	
+    //print(x);
+    Date tommorow;
+    tommorow.day = 15;
+    tommorow.month = 1;
+    tommorow.year = 2022;
+    print(tommorow);
+
+
+
 	std::count<<x.day<<"."<<x.month<<"."<<x.year << std::enl; 
     return a.exec();
 }
