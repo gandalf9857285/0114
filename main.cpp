c4d23bd89ccdb94b33d1e46140dd73e972e49778
Valentin Romanov <valentin1.romanov@yandex.ru>
2022-01-14 10:50:36 +0700

    структуры

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <QCoreApplication>
+struct Date{
+	int day;
+	int month;
+	int year;
 
+};
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+	 Date x;
+	x.day = 14;
+	x.month = 1;
+	x.year = 2022;
+	
+	std::count<<x.day<<"."<<x.month<<"."<<x.year << std::enl; 
     return a.exec();
 }
